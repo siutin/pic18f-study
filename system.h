3e0fe6fa17ce814ab1faea685b827d041cdad384
osiutino <osiutino@gmail.com>
2017-01-28 17:25:08 +0800

    fixup! fixup! make HSPLL works with Proteus

--- system.h
+++ system.h
@@ -5,9 +5,10 @@
 /* TODO Define system operating frequency */
 
 /* Microcontroller MIPs (FCY) */
-#define SYS_FREQ        8000000L
+#define SYS_FREQ        32000000L
 #define FCY             SYS_FREQ/4
 
+#define INT_OSC            8000000L
 #define RATIO              4    // if ppl enabled: 4, otherwise 0
 
 /******************************************************************************/
